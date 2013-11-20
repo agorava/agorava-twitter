6bba5b79a2f6500baba6061d9bd4d8cfe3e47499
Antoine Sabot-Durand <antoine@sabot-durand.net>
2013-11-20 08:51:44 +0100

    Switching twitter API to use SSL and Authentication URL

--- agorava-twitter-api/src/main/java/org/agorava/twitter/TwitterApi.java
+++ agorava-twitter-api/src/main/java/org/agorava/twitter/TwitterApi.java
@@ -32,20 +32,22 @@ public class TwitterApi extends ProviderConfigOauth10a {
 
     private static final String MEDIA_NAME = "Twitter";
 
-    private static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token=%s";
+    //private static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token=%s";
 
-    private static final String REQUEST_TOKEN_RESOURCE = "api.twitter.com/oauth/request_token";
+    private static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token=%s";
 
-    private static final String ACCESS_TOKEN_RESOURCE = "api.twitter.com/oauth/access_token";
+    private static final String REQUEST_TOKEN_RESOURCE = "https://api.twitter.com/oauth/request_token";
+
+    private static final String ACCESS_TOKEN_RESOURCE = "https://api.twitter.com/oauth/access_token";
 
     @Override
     public String getAccessTokenEndpoint() {
-        return "http://" + ACCESS_TOKEN_RESOURCE;
+        return ACCESS_TOKEN_RESOURCE;
     }
 
     @Override
     public String getRequestTokenEndpoint() {
-        return "http://" + REQUEST_TOKEN_RESOURCE;
+        return REQUEST_TOKEN_RESOURCE;
     }
 
     @Override
@@ -54,45 +56,6 @@ public class TwitterApi extends ProviderConfigOauth10a {
     }
 
 
-      /*public static class SSL extends TwitterApi
-      {
-        @Override
-        public String getAccessTokenEndpoint()
-        {
-          return "https://" + ACCESS_TOKEN_RESOURCE;
-        }
-
-        @Override
-        public String getRequestTokenEndpoint()
-        {
-          return "https://" + REQUEST_TOKEN_RESOURCE;
-        }
-      }
-
-      *//**
-     * Twitter 'friendlier' authorization endpoint for OAuth.
-     *
-     * Uses SSL.
-     *//*
-      public static class Authenticate extends SSL
-      {
-        private static final String AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token=%s";
-
-        @Override
-        public String startDanceFor(Token requestToken)
-        {
-          return String.format(AUTHENTICATE_URL, requestToken.getToken());
-        }
-      }
-
-      */
-
-    /**
-     * Just an alias to the default (SSL) authorization endpoint.
-     * <p/>
-     * Need to include this for symmetry with 'Authenticate' only.
-     *//*
-      public static class Authorize extends SSL{}*/
     @Override
     public String getProviderName() {
         return MEDIA_NAME;
